51442cc4f6a625c25bf82711ebfb6d7a4e85d9d4
dlynquent <leonkabdool@gmail.com>
2021-07-23 21:55:04 -0400

    Made changes to login page wanted a different aesthetic

---
+++ include/head.php
@@ -0,0 +1,16 @@
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover">
+    <!--meta http-equiv="X-UA-Compatible" content="ie=edge"-->
+    <title>QWIXS</title>
+    <link rel="shortcut icon" href="assets/">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <!-- Font Awesome CSS -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet">
+    <!-- Style CSS -->
+    <link rel="stylesheet" href="css/style.css">
+    <!-- Google Fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap" rel="stylesheet">
+
+</head>
--- login.php
+++ login.php
@@ -8,80 +8,82 @@ session_start();
 $pageID = "Login";
 
 // Check if the user is already logged in, if yes then redirect him to welcome page
-if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true){
-  header("location: index.php");
-  exit;
+if (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true) {
+    header("location: index.php");
+    exit;
 }
- 
+
 // Include config file
 require_once "config.php";
- 
+
 // Define variables and initialize with empty values
 $email = "";
 $password = "";
 $email_err = "";
 $password_err = "";
- 
+
 // Processing form data when form is submitted
-if($_SERVER["REQUEST_METHOD"] == "POST"){
- 
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+
+
     // Check if email is empty
-    if(empty(trim($_POST["email"]))){
+    if (empty(trim($_POST["email"]))) {
         $email_err = "Please enter email.";
-    } else{
+    } else {
         $email = trim($_POST["email"]);
     }
-    
+
     // Check if password is empty
-    if(empty(trim($_POST["password"]))){
+    if (empty(trim($_POST["password"]))) {
         $password_err = "Please enter your password.";
-    } else{
+    } else {
         $password = trim($_POST["password"]);
     }
-    
+
     // Validate credentials
-    if(empty($email_err) && empty($password_err)){
+    if (empty($email_err) && empty($password_err)) {
         // Prepare a select statement
         $sql = "SELECT 'user_id', email, password FROM users WHERE email = ?";
-        
-        if($stmt = mysqli_prepare($link, $sql)){
+
+        if ($stmt = mysqli_prepare($link, $sql)) {
             // Bind variables to the prepared statement as parameters
             mysqli_stmt_bind_param($stmt, "s", $param_email);
-            
+
             // Set parameters
             $param_email = $email;
-            
+
             // Attempt to execute the prepared statement
-            if(mysqli_stmt_execute($stmt)){
+            if (mysqli_stmt_execute($stmt)) {
                 // Store result
                 mysqli_stmt_store_result($stmt);
-                
+
                 // Check if email exists, if yes then verify password
-                if(mysqli_stmt_num_rows($stmt) == 1){                    
+                if (mysqli_stmt_num_rows($stmt) == 1) {
                     // Bind result variables
                     mysqli_stmt_bind_result($stmt, $id, $email, $hashed_password);
-                    if(mysqli_stmt_fetch($stmt)){
-                        if(password_verify($password, $hashed_password)){
+                    if (mysqli_stmt_fetch($stmt)) {
+                        if (password_verify($password, $hashed_password)) {
                             // Password is correct, so start a new session
                             session_start();
-                            
+
                             // Store data in session variables
                             $_SESSION["loggedin"] = true;
                             $_SESSION["id"] = $id;
-                            $_SESSION["email"] = $email;                            
-                            
+                            $_SESSION["email"] = $email;
+
                             // Redirect user to welcome page
                             header("location: homepage.php");
-                        } else{
+                        } else {
                             // Display an error message if password is not valid
                             $password_err = "The password you entered was not valid.";
                         }
                     }
-                } else{
+                } else {
                     // Display an error message if email doesn't exist
                     $email_err = "No account found with that email.";
                 }
-            } else{
+            } else {
                 echo "Oops! Something went wrong. Please try again later.";
             }
 
@@ -89,7 +91,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
             mysqli_stmt_close($stmt);
         }
     }
-    
+
     // Close connection
     mysqli_close($link);
 }
@@ -99,43 +101,64 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
 
 <head>
     <title>QWIXS Login</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+    <?php include 'include/head.php'; ?>
+    <!-- Fontawesome -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css">
 </head>
 
 <body>
-    <section class="container-fluid">
-        <section class="row justify-content-center">
-            <section class="col-12 col-sm-6 col-md-3">
-                <form class "form-container" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>"
-                    method="post">
-                    <h4 class="text-center font-weight-bold">QWIXS Sign-In</h4>
-                    <div class=" mb-3 form-group ab-empty-item<?php echo (!empty($email_err)) ? 'has-error' : ''; ?>">
-
-                        <input type="email" class="form-control" name="email" class="form-control"
-                            placeholder="Enter Email" value="<?php echo $email; ?>">
-                        <span class="help-block"><?php echo $email_err; ?></span>
+    <div class="container px-1 px-md-5 px-lg-1 px-xl-5 py-5 mx-auto">
+        <div class="card card0 border-0">
+            <div class="row d-flex">
+                <div class="col-lg-6">
+                    <div class="card1 pb-5">
+                        <div class="row"> <img src="https://i.imgur.com/CXQmsmF.png" class="logo"> </div>
+                        <div class="row px-3 justify-content-center mt-4 mb-5 border-line"> <img
+                                src="https://i.imgur.com/uNGdWHi.png" class="image"> </div>
                     </div>
-                    <div class="mb-3 form-group <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
-                        <div class="forgot">
-                            <a href="">Forgot Password?</a>
+                </div>
+                <div class="col-lg-6">
+                    <form action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" method="post">
+                        <div class="card2 card border-0 px-4 py-5">
+                            <div class="row px-3"> <label class="mb-1"
+                                    <?php echo (!empty($email_err)) ? 'has-error' : ''; ?>>
+                                    <h6 class="mb-0 text-sm">Email Address</h6>
+                                </label>
+                                <input class="mb-4" type="email" name="email" placeholder="Enter a valid email address"
+                                    value="<?php echo $email; ?>">
+                                <span class="help-block"><?php echo $email_err; ?></span>
+                            </div>
+                            <div class="row px-3"> <label class="mb-1"
+                                    <?php echo (!empty($password_err)) ? 'has-error' : ''; ?>">
+                                    <h6 class="mb-0 text-sm">Password</h6>
+                                </label> <input type="password" name="password" placeholder="Enter password">
+                                <span class="help-block"><?php echo $password_err; ?></span>
+                            </div>
+                            <div class="row px-3 mb-4">
+                                <div class="custom-control custom-checkbox custom-control-inline"> <input id="chk1"
+                                        type="checkbox" name="chk" class="custom-control-input"> <label for="chk1"
+                                        class="custom-control-label text-sm">Remember me</label> </div> <a href="#"
+                                    class="ml-auto mb-0 text-sm">Forgot Password?</a>
+                            </div>
+                            <div class="row mb-3 px-3"> <button type="submit"
+                                    class="btn btn-blue text-center">Login</button> </div>
+                            <div class="row mb-4 px-3"> <small class="font-weight-bold">Don't have an account? <a
+                                        class="text-danger ">Register</a></small> </div>
                         </div>
-                        <input type="password" name="password" class="form-control" placeholder="Enter Password">
-                        <span class="help-block"><?php echo $password_err; ?></span>
-                    </div>
-                    <div class="mb-3 form-check">
-                        <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                        <label class="form-check-label" for="exampleCheck1">Remember me</label>
-                    </div>
-                    <button type="submit" class="btn btn-primary">Submit</button>
-                </form>
-            </section>
-        </section>
-
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js"
-            integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous">
-        </script>
+                    </form>
+                </div>
+            </div>
+            <div class="bg-blue py-4">
+                <div class="row px-3"> <small class="ml-4 ml-sm-5 mb-2">Copyright &copy; 2019. All rights
+                        reserved.</small>
+                    <div class="social-contact ml-4 ml-sm-auto"> <span class="fa fa-facebook mr-4 text-sm"></span> <span
+                            class="fa fa-google-plus mr-4 text-sm"></span> <span
+                            class="fa fa-linkedin mr-4 text-sm"></span> <span
+                            class="fa fa-twitter mr-4 mr-sm-5 text-sm"></span> </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
 </html>
